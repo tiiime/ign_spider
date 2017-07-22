1711b6a38e305f2dc5007c1a912cd0c9042f9c7e
kang <kjxstar@gmail.com>
2017-07-22 13:59:24 +0800

    item

--- ign/items.py
+++ ign/items.py
@@ -11,4 +11,11 @@ import scrapy
 class IgnItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    cover = scrapy.Field()
+    title = scrapy.Field()
+    platform = scrapy.Field()
+    type = scrapy.Field()
+    details = scrapy.Field()
+    date = scrapy.Field()
+    score = scrapy.Field()
+    scorePhrase = scrapy.Field()
--- ign/spiders/ign_spider.py
+++ ign/spiders/ign_spider.py
@@ -1,5 +1,7 @@
 import scrapy
 
+from ign.items import IgnItem
+
 
 class IGNSpider(scrapy.Spider):
     name = "IGN_Spider"
@@ -13,5 +15,16 @@ class IGNSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        self.log(response)
-        self.log("get")
+        items = []
+        for sel in response.css('body > div'):
+            item = IgnItem()
+            item['cover'] = sel.xpath('div[1]/a/img/@src').extract_first().strip()
+            item['title'] = sel.xpath('div[2]/div[1]/h3/a/text()').extract_first().strip()
+            item['platform'] = sel.xpath('div[2]/div[1]/h3/span/text()').extract_first().strip()
+            item['type'] = sel.xpath('div[2]/p/span/text()').extract_first().strip()
+            item['details'] = sel.xpath('div[2]/p/text()').extract_first().strip()
+            item['date'] = sel.xpath('div[3]/div/text()').extract_first().strip()
+            item['score'] = sel.xpath('div[4]/div/a/span[1]/text()').extract_first().strip()
+            item['scorePhrase'] = sel.xpath('div[4]/div/a/span[2]/text()').extract_first().strip()
+            items.append(item)
+        return items
